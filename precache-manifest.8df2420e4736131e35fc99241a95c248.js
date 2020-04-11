7b288ec3be9c75fed4e610e64de24b641d4dfeb8
Karen Garcia <kgarcia@i3screen.com>
2020-04-11 09:17:04 -0600

    Updates

---
+++ precache-manifest.8df2420e4736131e35fc99241a95c248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c60446110253d7915453ab4f18d563",
+    "url": "/conquer/index.html"
+  },
+  {
+    "revision": "144dc59497d35722686d",
+    "url": "/conquer/static/css/main.4b6e1394.chunk.css"
+  },
+  {
+    "revision": "4f62d6e96f1e6bf8e5fc",
+    "url": "/conquer/static/js/2.745015b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conquer/static/js/2.745015b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144dc59497d35722686d",
+    "url": "/conquer/static/js/main.8aa10c01.chunk.js"
+  },
+  {
+    "revision": "79b754681bb508f49647",
+    "url": "/conquer/static/js/runtime-main.2f13256d.js"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/conquer/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
--- src/components/app/numberConverter.js
+++
@@ -1,5 +0,0 @@
-const numberConverter =(value) =>{
-    return parseFloat((value).toFixed(2));
-};
-
-export default numberConverter;
